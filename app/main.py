84060839034ef3066b0c1878366b4ffb872168ae
Sann-Htet <sannhtet899@gmail.com>
2023-12-18 16:37:23 +0630

    hashing user password and returning a user profile

--- app/main.py
+++ app/main.py
@@ -6,8 +6,9 @@ from psycopg2.extras import RealDictCursor
 import psycopg2
 import time
 from sqlalchemy.orm import Session
-from . import models, schema
+from . import models, schema, utils
 from .database import engine, get_db
+
 
 models.Base.metadata.create_all(bind=engine)
 
@@ -91,3 +92,25 @@ def update_post(id: int, post: schema.PostCreate, db: Session = Depends(get_db),
     db.commit()
 
     return post_query.first()
+
+
+@app.post("/users", status_code=status.HTTP_201_CREATED, response_model=schema.UserOut)
+def create_user(user: schema.UserCreate, db: Session = Depends(get_db)):
+    # hash the password  - user.password
+    hash_password = utils.hash(user.password)
+    user.password = hash_password
+    new_user = models.User(**user.model_dump())
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+
+    return new_user
+
+@app.get("/users/{id}", response_model=schema.UserOut)
+def get_user(id: int, db: Session = Depends(get_db)):
+    user = db.query(models.User).filter(models.User.id == id).first()
+
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"User with id: {id} does not exist")
+    
+    return user
