0863e2a17e407266e22e7f02620038714421e7d9
Sann-Htet <sannhtet899@gmail.com>
2023-12-19 14:05:38 +0630

    adding search query parameter

--- app/routers/post.py
+++ app/routers/post.py
@@ -2,20 +2,18 @@ from fastapi import FastAPI, Response, status, HTTPException, Depends, APIRouter
 from sqlalchemy.orm import Session
 from .. import models, schema, oauth2
 from ..database import get_db
-from typing import List
+from typing import List, Optional
 
 router = APIRouter(
     prefix="/posts",
     tags=['Posts']
 )
 
-
-
 @router.get("/", response_model=List[schema.Post])
-def get_posts(db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user), limit: int = 10, skip: int = 0):
+def get_posts(db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user), limit: int = 10, skip: int = 0, search: Optional[str] = ""):
     # cursor.execute("""SELECT * FROM posts""")
     # posts = cursor.fetchall()
-    posts = db.query(models.Post).limit(limit).offset(skip).all()
+    posts = db.query(models.Post).filter(models.Post.title.contains(search)).limit(limit).offset(skip).all()
     return posts
 
 @router.get("/{id}", response_model=schema.Post) # Path parameter
