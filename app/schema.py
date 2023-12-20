10f9977d6cb9cb04ec461ed7c9ef949b3b08dd89
Sann-Htet <sannhtet899@gmail.com>
2023-12-21 01:15:56 +0630

    create a voting table

--- app/schema.py
+++ app/schema.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel, EmailStr
+from pydantic import BaseModel, EmailStr, conint
 from datetime import datetime
 from typing import Optional
 
@@ -49,3 +49,7 @@ class Token(BaseModel):
 
 class TokenData(BaseModel):
     id: Optional[int] = None
+
+class Vote(BaseModel):
+    post_id: int
+    dir: conint(le=1)
